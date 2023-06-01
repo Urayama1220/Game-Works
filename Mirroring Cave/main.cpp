88905eca2cbcfcef2ef1655338a91a48e7cf9cc5
ama1220 <82570603+Urayama1220@users.noreply.github.com>
2023-06-01 14:30:50 +0900

    Add files via upload

---
+++ Mirroring Cave/main.cpp
@@ -0,0 +1,16 @@
+#include<DxLib.h>
+#include "class/Scene/SceneMng.h"
+#include "_debug/_DebugConOut.h"
+#include "_debug/_DebugDispOut.h"
+
+
+int WINAPI WinMain(HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine,
+	int nCmdShow)
+{
+	lpSceneMng.Run();
+
+
+	return 0;
+}
